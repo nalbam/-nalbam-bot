fe73e9f6fd23800cd16a8c7a3d3c4935d120c46c
nalbam-ya <jyyu@yanolja.com>
2017-11-02 00:59:45 +0900

    upload

--- src/main/java/com/nalbam/bot/service/AmazonServiceImpl.java
+++ src/main/java/com/nalbam/bot/service/AmazonServiceImpl.java
@@ -3,6 +3,7 @@ package com.nalbam.bot.service;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.model.*;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -52,28 +52,21 @@ public class AmazonServiceImpl implements AmazonService {
 
     @Override
     public PutObjectResult upload(final MultipartFile multipartFile) {
-        final File file = getFile(multipartFile);
-
-        return upload(file);
-    }
-
-    private PutObjectResult upload(final File file) {
-        final String exe = FilenameUtils.getExtension(file.getName()).toLowerCase();
+        final String exe = FilenameUtils.getExtension(multipartFile.getOriginalFilename()).toLowerCase();
         final String key = UUID.randomUUID().toString() + "." + exe;
 
-        final PutObjectRequest request = new PutObjectRequest(this.bucket, key, file);
+        final ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(multipartFile.getContentType());
 
-        return this.amazonS3.putObject(request);
-    }
-
-    private File getFile(final MultipartFile multipart) {
-        final File file = new File(multipart.getOriginalFilename());
         try {
-            multipart.transferTo(file);
+            final PutObjectRequest request = new PutObjectRequest(this.bucket, key, multipartFile.getInputStream(), objectMetadata);
+
+            return this.amazonS3.putObject(request);
         } catch (final IOException e) {
             e.printStackTrace();
         }
-        return file;
+
+        return null;
     }
 
     private Image getImage(final String key) {
