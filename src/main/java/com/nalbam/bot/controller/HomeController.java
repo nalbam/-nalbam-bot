438ea29cc086f2b94698194be1e1c7f1996c9248
nalbam-ya <jyyu@yanolja.com>
2017-11-03 10:30:34 +0900

    health

--- src/main/java/com/nalbam/bot/controller/HomeController.java
+++ src/main/java/com/nalbam/bot/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.nalbam.bot.controller;
 
+import com.nalbam.common.util.PackageUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/health")
-    public String health() {
-        return "OK";
+    public Map<String, String> health() {
+        return PackageUtil.getData(this.getClass());
     }
 
 }
