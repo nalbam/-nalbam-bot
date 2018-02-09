08f36d3915e696d3fc49dcb1244f8364e53ca73b
nalbam <skfqka@80>
2018-02-09 11:50:05 +0900

    deploy-ecr.sh

---
+++ deploy-ecr.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+nothing() {
+    USERID=
+    REGION=
+}
+
+error() {
+    echo -e "$1"
+    exit 1
+}
+
+REGION=us-east-1
+
+ARTIFACT_ID=nalbam-bot
+VERSION=0.0.0
+
+if [ "${USERID}" != "" ]; then
+    REPOSITORY="${USERID}.dkr.ecr.${REGION}.amazonaws.com"
+fi
+
+if [ "${REPOSITORY}" == "" ]; then
+    error "Not set REPOSITORY."
+fi
+
+echo_ "publish to docker... [${REPOSITORY}]"
+
+docker version
+
+pushd target/docker
+
+echo_ "docker build... [${ARTIFACT_ID}]"
+
+docker build --rm=false -t ${REPOSITORY}/${ARTIFACT_ID}:${VERSION} .
+
+docker images
+
+echo_ "docker login..."
+
+ECR_LOGIN=$(aws ecr get-login --region ${REGION} --no-include-email)
+#${ECR_LOGIN}
+
+echo_ "docker push... [${ARTIFACT_ID}]"
+
+docker push ${REPOSITORY}/${ARTIFACT_ID}:${VERSION}
+
+popd
